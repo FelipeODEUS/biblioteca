4d322dff73a55eb9fdb366d274bf1c29448cba03
FelipeODEUS <92820937+FelipeODEUS@users.noreply.github.com>
2022-05-20 18:17:20 -0300

    biblioteca 2

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Navegadordeabas from './components/Navegadordeabas';
+
+export default class App extends Component {
+  render(){
+    return <Navegadordeabas/>
+  }
+}
+
+
---
+++ screens/teladetransacao.js
@@ -0,0 +1,85 @@
+import React, {Component} from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import  * as Permissions from "expo-permissions"
+import { BarCodeScanner } from "expo-barcode-scanner";
+
+export default class Teladetransacao extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            stateMode: "normal",
+            permissionsCamera: null,
+            scanner: false,
+            dateScanner: ""
+        }
+    }
+
+    getCameraPermissions = async stateMode => {
+        const{status} = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({
+            PermissionsCamera: status === "granted",
+            stateMode: stateMode,
+            scanner: false
+            
+        })
+    }
+
+    handleBarCodeScanned = async({type, data}) => {
+        this.setState({
+            dateScanner: data,
+            stateMode: "normal",
+            scanner: true
+        })
+    }
+    render(){
+        const{ stateMode, permissionsCamera, dateScanner, scanner} = this.state;
+        if(stateMode === "scanner"){
+            return(
+                <BarCodeScanner
+                onBarCodeScanned={scanner ? undefined : this.handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+                />
+            )
+        }
+        return(
+            <View style={styles.container}>
+                <Text style = {styles.text}>
+                    {permissionsCamera ? dateScanner : "solicitar a permição para a camera"};
+                </Text>
+                <TouchableOpacity style={styles.button}
+                onPress = {() => this.getCameraPermissions("scanner")}
+                >
+                    <Text style={styles.buttonText}> Leia o QR code</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#5653D4"
+    },
+    text: {
+        color: "#fff",
+        fontSize: 30
+    },
+    button: {
+        width: "43%",
+        height: 80,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor:"#F48D20",
+        borderRadius: 15
+    },
+    buttonText:{
+        fontSize: 24,
+        color:"#FFFFFF"
+    }
+})
+
+
+
